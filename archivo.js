b21718b1c47d8b85430dd30dec1d9fe17c4f476a
Yojan Stiben Hungría <69788548+yshungria-lab@users.noreply.github.com>
2020-09-25 20:38:31 -0500

    nuevo archivo

--- archivo.js
+++ archivo.js
@@ -1,10 +1,2 @@
-<< << << < HEAD
 alert("Hola Yojan Stiben");
-console.log("cambios en el archivo") ===
-    ===
-    =
-    alert("Hola Yojan Stiben);
-        console.log("cambios en el archivo") >>>
-        >>>
-        >
-        ada94a8...Update archivo.js
+console.log("cambios en el archivo");
